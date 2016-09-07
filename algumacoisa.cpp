129bf2a8936e40318478db65b62288ba8b286939
Your Name <willpereirabr@gmail.com>
2016-09-06 23:19:32 -0300

    06/09/2016

--- algumacoisa.cpp
+++ algumacoisa.cpp
@@ -13,11 +13,13 @@
 
 struct Particula{
 	float x,y,velocidadeX,velocidadeY;
+	bool ativa;
 }particulas[NUM_PARTICULAS];
 
 void render();
 void InicGL(int Largura, int Altura);
 void InicParticulas();
+void ReinicParticula(int i);
 
 int main(int argc, char** argv){
 	glutInit(&argc,argv);
@@ -30,6 +32,7 @@ int main(int argc, char** argv){
 	glutIdleFunc(render);
 	InicGL(LARGURA,ALTURA);	
 	glutMainLoop();
+	return 0;
 }
 
 
@@ -37,13 +40,18 @@ void render(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glBegin(GL_LINES);
 	for(int i = 0; i<NUM_PARTICULAS; i++){
-		float x_anterior = particulas[i].x;
-		float y_anterior = particulas[i].y;
-		particulas[i].x += particulas[i].velocidadeX;
-		particulas[i].y += particulas[i].velocidadeY;
-		glVertex2f(x_anterior,y_anterior);
-		glVertex2f(particulas[i].x,particulas[i].y);
+		if(particulas[i].ativa){	
+			float x_anterior = particulas[i].x;
+			float y_anterior = particulas[i].y;
+			particulas[i].x += particulas[i].velocidadeX;
+			particulas[i].y += particulas[i].velocidadeY;
+			glVertex2f(x_anterior,y_anterior);
+			glVertex2f(particulas[i].x,particulas[i].y+0.05);
+			if(y_anterior<=-1)
+				ReinicParticula(i);
+		}
 	}
+
 	glEnd();
 	// printf("--oi--\n\n");
 	glutSwapBuffers();
@@ -66,13 +74,21 @@ void InicGL(int Largura, int Altura){
 
 void InicParticulas(){	
 	for(int i=0;i<NUM_PARTICULAS;i++){
-		float velocidade = (float)(rand() % 100)/5000 +0.01;
-		int angulo = 80;
+		float velocidade = (float)(rand() % 1000)/100000 +0.001;
 		particulas[i].velocidadeX = velocidade/10;
 		particulas[i].velocidadeY = -velocidade;
 		particulas[i].x = (rand() % 1000)/500.0f-1;
-		particulas[i].y = 0.9;
+		particulas[i].y = 1;
+		particulas[i].ativa = 1;
 		// particulas[i].x = 0.5f;
 		// particulas[i].y = 0.5f;
 	}
+}
+
+void ReinicParticula(int i){	
+	float velocidade = (float)(rand() % 1000)/100000 +0.001;
+	particulas[i].velocidadeX = velocidade/10;
+	particulas[i].velocidadeY = -velocidade;
+	particulas[i].x = (rand() % 1000)/500.0f-1;
+	particulas[i].y = 1;
 }
